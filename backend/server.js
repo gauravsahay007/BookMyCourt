05a759ce5ad8fe7dce4c0267fd1a93b8ff1b857a
Gaurav <97462628+gauravsahay007@users.noreply.github.com>
2024-10-17 05:14:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json()); // For parsing application/json
